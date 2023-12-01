187328a00804cf86171c30925d12a89706cb576e
yunssu <soneymarr@gmail.com>
2023-12-01 20:23:34 +0900

    FlutterBoot 5일차

--- lib/day5_basic.dart
+++ lib/day5_basic.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SpaceObjectsScreen(),
-    );
-  }
-}
-
 class SpaceObjectsScreen extends StatefulWidget {
   @override
   _SpaceObjectsScreenState createState() => _SpaceObjectsScreenState();
